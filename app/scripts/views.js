01de3cfca5c72098b8bf5fcf62488482f006b101
Joe Tamburro <joetamburro38@gmail.com>
2013-10-21 15:05:16 -0400

    removed jplayer

--- app/scripts/views.js
+++ app/scripts/views.js
@@ -1,16 +1,16 @@
-HomeView = Backbone.View.extend({
+// HomeView = Backbone.View.extend({
 
-  template: _.template( $('#home-view-template').text() )
+//   template: _.template( $('#home-view-template').text() )
 
 
-  initialize: function(){
-    $('.content-area').append(this.el)
-    this.render()
-  },
+//   initialize: function(){
+//     $('.content-area').append(this.el)
+//     this.render()
+//   },
 
-  render: function(){
-    // this.$el.attr('href', "#/students/"+ this.model.get('_id') )
-    this.$el.append( this.template({item: this.model }) )
-  },
+//   render: function(){
+//     // this.$el.attr('href', "#/students/"+ this.model.get('_id') )
+//     this.$el.append( this.template({item: this.model }) )
+//   },
 
-})
+// })
