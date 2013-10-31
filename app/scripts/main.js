6bee6beb0970ce38105bb6359520637953fa3042
joetamburro <joetamburro38@gmail.com>
2013-10-31 17:27:28 -0400

    cleared out main js file

--- app/scripts/main.js
+++
@@ -1,7 +0,0 @@
-
-  navSelection: function (event){
-    $('.navigation-bar li').removeClass('current-tab')
-    $(event.currentTarget).addClass('current-tab')
-  }
-
-  $('.navigation-bar li').navSelection()
